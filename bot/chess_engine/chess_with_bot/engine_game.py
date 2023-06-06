72781667251da913a2282c8786b80d13793eef1d
Dmitriy <112427972+pulivilizator@users.noreply.github.com>
2023-06-06 16:17:56 +0300

    out of memory stockfish

--- bot/chess_engine/chess_with_bot/engine_game.py
+++ bot/chess_engine/chess_with_bot/engine_game.py
@@ -6,9 +6,8 @@ from interface import keyboards
 from chess_engine.svg_to_png import svg_to_png
 
 
-
+engine = chess.engine.SimpleEngine.popen_uci(r"../stockfish_linux/stockfish-ubuntu.04-x86-64")
 async def play_game(board: chess.Board, move: str, user) -> int | bool | keyboards.InlineKeyboard:
-    engine = chess.engine.SimpleEngine.popen_uci(r"../stockfish_linux/stockfish-ubuntu.04-x86-64")
     if board.turn == chess.WHITE:
 
         if move in [str(m) for m in board.legal_moves]:
@@ -18,13 +17,11 @@ async def play_game(board: chess.Board, move: str, user) -> int | bool | keyboar
         else:
             return False, keyboards.InlineKeyboard.create_inline_keyboard(board)
         if board.is_game_over():
-            engine.quit()
             return -2
     result = engine.play(board, chess.engine.Limit(time=0.1))
     board.push(result.move)
     await svg_to_png(board, user)
     if board.is_game_over():
-        engine.quit()
         return -1
 
     return keyboards.InlineKeyboard.create_inline_keyboard(board)
